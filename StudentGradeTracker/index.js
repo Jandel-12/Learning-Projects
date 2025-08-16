b075d6379bdf71b5d2d4fef41ac18ae854e8f802
jandel-12 <177752563+Jandel-12@users.noreply.github.com>
2025-08-16 17:27:20 +0800

    added simple UI

--- StudentGradeTracker/index.js
+++ StudentGradeTracker/index.js
@@ -1,3 +1,5 @@
+
+
 class Student{
     constructor(name){
         this.id = crypto.randomUUID();
@@ -22,6 +24,47 @@ class GradeBook{
     getStudentById(studentId){
         return this.allStudents.find(student => student.id === studentId);
     }
+    
+    studentDisplay() {
+    const main = document.querySelector(".main-container");
+    main.innerHTML = "";
+
+    this.allStudents.forEach(student => {
+        let studentContainer = document.createElement("div");
+        studentContainer.classList.add("student-container");
+        main.appendChild(studentContainer);
+
+        let studentName = document.createElement("h4");
+        studentName.textContent = student.name;
+        studentContainer.appendChild(studentName);
+
+        for (let sub in student.subject) {
+            let subjectContainer = document.createElement("div");
+            subjectContainer.classList.add("subject");
+
+            
+            let subjectTitle = document.createElement("p");
+            subjectTitle.textContent = `${sub}: ${student.subject[sub].join(", ")}`;
+            subjectContainer.appendChild(subjectTitle);
+
+            
+            const removeSub = document.createElement("button");
+            removeSub.classList.add("remove");
+            removeSub.textContent = "Remove Subject";
+
+           
+            removeSub.addEventListener("click", () => {
+                delete student.subject[sub]; 
+                this.studentDisplay();       
+            });
+
+            subjectContainer.appendChild(removeSub);
+            studentContainer.appendChild(subjectContainer);
+        }
+    });
+}
+
+ 
 
 }
 
@@ -52,6 +95,10 @@ class App{
         student.subject[sub].push(grades);
     }
 
+    studentDisplay(){
+        this.studentList.studentDisplay()
+    }
+
 }
 
 const app = new App();
@@ -61,5 +108,5 @@ app.addSubjects(studentId, "Math", 95);
 app.addSubjects(studentId, "Math", 88);
 app.addSubjects(studentId, "English", 90);
 
-console.log(app.studentList.allStudents);
+app.studentDisplay();
 
